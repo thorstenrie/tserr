85fcb9710ab48cb07065c98465f060cbc56297b4
thorstenrie <99222474+thorstenrie@users.noreply.github.com>
2022-10-05 23:00:19 +0200

    Fixed format issue

--- tserr.go
+++ tserr.go
@@ -24,8 +24,7 @@
 //
 //     {"error":{"id":6,"code":500,"message":"test1 does not equal test2"}}
 //
-// Copyright (c) 2022 thorstenrie
-//
+// Copyright (c) 2022 thorstenrie.
 // All Rights Reserved. Use is governed with GNU Affero General Public License v3.0
 // that can be found in the LICENSE file.
 package tserr
